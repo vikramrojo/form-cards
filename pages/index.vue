2320777e481e49b983c71ef0091b27bb6b0b84dd
atav1k <atavikram@gmail.com>
2018-09-21 11:54:42 -0700

    signup

--- pages/index.vue
+++ pages/index.vue
@@ -9,9 +9,15 @@
       .row
         .col6.center
           img(src="/img/cards.png" data-emergence="hidden").isAnimated
-        .col6.marT6.marReset
+        .col6.marT3.marReset
           h2.bolder.indigo Form Cards
           h3 Design principles and prompts for your product team to evaluate user interfaces.
+          p *The inks are still wet but if you'd like a sneak peek, signup and we'll host a short workshop!
+          no-ssr
+            form(data-netlify="true" name="contact" method="POST" action="http://form.cards/thanks.html")
+              .inputGroup
+                input(type="email" name="email" placeholder="Email")
+                button(type="submit") Signup
     section#claim
       .row.padY4.padX7.center.padReset
         .col
